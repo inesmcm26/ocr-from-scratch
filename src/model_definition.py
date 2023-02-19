7ab081c27e4fdcc79bde7b49b42ccce28271c69a
inesmcm <ines.magessi@tecnico.ulisboa.pt>
2023-02-19 15:13:49 +0000

    new yolov1 model definition

--- src/model_definition.py
+++ src/model_definition.py
@@ -1,88 +1,98 @@
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, UpSampling2D, concatenate
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
+import keras.backend as K
+from keras.models import Model
+from keras.layers import Dense, Input, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.regularizers import l2
 
-def UNet(pretrained_weigths = None, input_size = (512, 512, 1)):
-  inputs = Input(input_size)
+class Yolo_Reshape(tf.keras.layers.Layer):
+  """
+  Reshapes the output of the YOLO model to the required output shape
+  """
 
-  conv1 = Conv2D(filters = 64, kernel_size = 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
-  conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+  def __init__(self, target_shape):
+    super(Yolo_Reshape, self).__init__()
+    self.target_shape = tuple(target_shape)
 
-  pool1 = MaxPooling2D(pool_size = (2, 2))(conv1)
+  def get_config(self):
+    config = super().get_config().copy()
+    config.update({
+        'target_shape': self.target_shape
+    })
+    return config
 
-  conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
-  conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+  def call(self, input):
+    # grids 15x15
+    S = [self.target_shape[0], self.target_shape[1]]
+    # classes
+    C = 1
+    # no of bounding boxes per grid
+    B = 2
 
-  pool2 = MaxPooling2D(pool_size = (2, 2))(conv2)
+    idx1 = S[0] * S[1] * C
+    idx2 = idx1 + S[0] * S[1] * B
+    
+    # class probabilities
+    class_probs = K.reshape(input[:, :idx1], (K.shape(input)[0],) + tuple([S[0], S[1], C]))
+    class_probs = K.softmax(class_probs)
 
-  conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
-  conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    #confidence
+    confs = K.reshape(input[:, idx1:idx2], (K.shape(input)[0],) + tuple([S[0], S[1], B]))
+    confs = K.sigmoid(confs)
 
-  pool3 = MaxPooling2D(pool_size = (2, 2))(conv3)
+    # boxes
+    boxes = K.reshape(input[:, idx2:], (K.shape(input)[0],) + tuple([S[0], S[1], B * 4]))
+    boxes = K.sigmoid(boxes)
 
-  conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
-  conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
-
-  drop4 = Dropout(0.5)(conv4)
+    outputs = K.concatenate([class_probs, confs, boxes])
+    return outputs
   
-  pool4 = MaxPooling2D(pool_size = (2, 2))(drop4)
 
-  conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
-  conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+def yolov1(input_shape = (418, 418, 3)):
 
-  drop5 = Dropout(0.5)(conv5)
+    lrelu = tf.keras.layers.LeakyReLU(alpha=0.1)
 
-  up6 = UpSampling2D(size = (2,2))(drop5)
-  conv6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(up6)
-  merge6 = concatenate([drop4, conv6], axis = 3)
-  conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
-  conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+    inpt = Input(input_shape)
+    conv1 = Conv2D(filters=64, kernel_size= 7, strides= 1, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(inpt)
+    max1 = MaxPooling2D(pool_size= 2, strides = 2, padding = 'same')(conv1)
 
-  up7 = UpSampling2D(size = (2,2))(conv6)
-  conv7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(up7)
-  merge7 = concatenate([conv3, conv7], axis = 3)
-  conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
-  conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+    conv2 = Conv2D(filters=192, kernel_size= 3, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(max1)
+    max2 = MaxPooling2D(pool_size= 2, strides = 2, padding = 'same')(conv2)
 
-  up8 = UpSampling2D(size = (2,2))(conv7)
-  conv8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(up8)
-  merge8 = concatenate([conv2, conv8], axis = 3)
-  conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
-  conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+    conv3 = Conv2D(filters=128, kernel_size= 1, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(max2)
+    conv3 = Conv2D(filters=256, kernel_size= 3, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(conv3)
+    conv3 = Conv2D(filters=256, kernel_size= 1, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(conv3)
+    conv3 = Conv2D(filters=512, kernel_size= 3, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(conv3)
+    max3 = MaxPooling2D(pool_size= 2, strides = 2, padding = 'same')(conv3)
+    
+    conv4 = Conv2D(filters=256, kernel_size= 1, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(max3)
+    conv4 = Conv2D(filters=512, kernel_size= 3, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(conv4)
+    conv4 = Conv2D(filters=256, kernel_size= 1, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(conv4)
+    conv4 = Conv2D(filters=512, kernel_size= 3, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(conv4)
+    conv4 = Conv2D(filters=256, kernel_size= 1, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(conv4)
+    conv4 = Conv2D(filters=512, kernel_size= 3, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(conv4)
+    conv4 = Conv2D(filters=256, kernel_size= 1, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(conv4)
+    conv4 = Conv2D(filters=512, kernel_size= 3, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(conv4)
+    conv4 = Conv2D(filters=512, kernel_size= 1, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(conv4)
+    conv4 = Conv2D(filters=1024, kernel_size= 3, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(conv4)
+    max4 = MaxPooling2D(pool_size= 2, strides = 2, padding = 'same')(conv4)
 
-  up9 = UpSampling2D(size = (2, 2))(conv8)
-  conv9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(up9)
-  merge9 = concatenate([conv1, conv9], axis = 3)
-  conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
-  conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-  conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv5 = Conv2D(filters=512, kernel_size= 1, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(max4)
+    conv5 = Conv2D(filters=1024, kernel_size= 3, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(conv5)
+    conv5 = Conv2D(filters=512, kernel_size= 1, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(conv5)
+    conv5 = Conv2D(filters=1024, kernel_size= 3, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(conv5)
+    conv5 = Conv2D(filters=1024, kernel_size= 3, padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4))(conv5)
+    conv5 = Conv2D(filters=1024, kernel_size= 3, strides= 2, padding = 'same')(conv5)
 
-  conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
+    conv6 = Conv2D(filters=1024, kernel_size= 3, activation=lrelu, kernel_regularizer=l2(5e-4))(conv5)
+    conv6 = Conv2D(filters=1024, kernel_size= 3, activation=lrelu, kernel_regularizer=l2(5e-4))(conv6)
 
-  model = Model(inputs, conv10)
+    flat = Flatten()(conv6)
+    dense1 = Dense(512)(flat)
+    dense2 = Dense(1024)(dense1)
+    drop = Dropout(0.5)(dense2)
+    dense3 = Dense(2475, activation = 'sigmoid')(drop)
+    out = Yolo_Reshape(target_shape=(15,15,11))(dense3)
 
-  model.compile(optimizer = Adam(lr = 1e-2), loss = 'binary_crossentropy', metrics = ['accuracy'])
+    model = Model(inpt, out)
 
-  if (pretrained_weigths):
-    model.load_weights(pretrained_weigths)
-
-  return model
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return model
